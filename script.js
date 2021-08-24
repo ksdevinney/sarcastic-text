2bb54ba17d88dfc09fb238165cc9e17d97ef07f2
ksdevinney <katie@devinney.org>
2021-08-23 21:37:45 -0500

    inital files

---
+++ script.js
@@ -0,0 +1,7 @@
+const initialText = document.getElementById("input-text");
+// const alteredText = document.getElementById("output-text");
+const submit = document.getElementById("submit-button");
+
+function transformText(text) {
+    
+}
