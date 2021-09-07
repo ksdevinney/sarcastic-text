a5b647ec76d6485176f90cf03cecc1f0fb45725c
ksdevinney <katie@devinney.org>
2021-09-07 10:08:47 -0500

    console log caps change

--- script.js
+++ script.js
@@ -7,35 +7,46 @@ const submit = document.getElementById("submit-button");
 // console.log(initialText);
 const textToTransform = initialText.value;
 
+function transformText() {
+    // let length = text.length;
+
+    let inputValue = initialText.value;
+
+    console.log(inputValue.toUpperCase());
+
+    // let clearCaps = text.string.toLowerCase();
+
+    // console.log(clearCaps);
+
+    // for (let i = 0; i < inputValue.length; i++) {
+    //     if (inputValue[i] >= 'a' && inputValue[i] <= 'z') {
+    //         alteredText.push(String.fromCharCode(i) - 32);
+    //     } else if (inputValue[i] >= 'A' && inputValue[i] <= 'Z') {
+    //         alteredText.push(String.fromCharCode(i) + 32);
+    //     }
+    //     console.log(alteredText);
+    // }
+};
+
 function getText() {
     let inputValue = initialText.value;
     console.log(inputValue);
 }
 
-// function transformText(text) {
-//     let length = text.length;
-
-//     // let clearCaps = text.toLowerCase();
-
-//     // console.log(clearCaps);
-
-//     for (let i = 0; i < length; i++) {
-//         if (text[i] >= 'a' && text[i] <= 'z') {
-//             alteredText.write(String.String.fromCharCode(str.charCodeAt(i) - 32)
-//             );
-//         } else if (text[i] >= 'A' && text[i] <= 'Z') {
-//             alteredText.write(String.String.fromCharCode(str.charCodeAt(i) + 32)
-//             );
-//         }
-//         console.log(alteredText);
-//     }
-// };
-
 submit.onclick = function handleClick() {
     console.log("click");
     event.preventDefault();
     getText();
-    console.log(initialText);
-    console.log(textToTransform);
-    // transformText(initialText);
+    transformText();
 };
+
+// for (let i = 0; i < inputValue.length; i++) {
+//     if (inputValue[i] >= 'a' && inputValue[i] <= 'z') {
+//         alteredText.write(String.String.fromCharCode(str.charCodeAt(i) - 32)
+//         );
+//     } else if (inputValue[i] >= 'A' && inputValue[i] <= 'Z') {
+//         alteredText.write(String.String.fromCharCode(str.charCodeAt(i) + 32)
+//         );
+//     }
+//     console.log(alteredText);
+// }
