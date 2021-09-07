3397a7fe2b9cba07a17a0422cc5c4049482ca5bb
ksdevinney <katie@devinney.org>
2021-09-07 11:06:15 -0500

    transformation works as desired

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-const startingText = [];
 // get input
 const initialText = document.getElementById("input-text");
 const submit = document.getElementById("submit-button");
 
 function transformText() {
 
+    // p element for new text
     let alteredText = document.getElementById("output-text");
 
+    // text from input box- is .value really needed?
     let inputValue = initialText.value;
 
+    // make it all lower case before transforming
     let lowerCase = inputValue.toLowerCase();
 
-    console.log(lowerCase);
+    let newText = '';
 
-    alteredText.innerHTML = lowerCase;
+    for (let i = 0; i < lowerCase.length; i++) {
+        const current = lowerCase[i];
+
+        if(i % 2 === 0) {
+            newText += current;
+        } else {
+            newText += current.toUpperCase();
+        }
+    }
+
+    // put the new text on the page
+    alteredText.innerHTML = newText;
 };
 
 submit.onclick = function handleClick() {
