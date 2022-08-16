c863322b5ec7660b4e54a732941b613be34bbc3f
ksdevinney <katie@devinney.org>
2022-08-15 20:46:32 -0700

    comments for python version

--- python/sarcasm.py
+++ python/sarcasm.py
@@ -1,7 +1,23 @@
 def main():
     originalText = input("Enter a sentence or phrase. ")
 
+    # get input
+
+    # call function w/ input
+
+# input change function
+
+    # variable for output
     newText = ""
+
+    # change entire string to lower case
+
+    # iterate through input
+
+    # capitalize every other letter, add to string
+    # leave punctuation alone
+
+    # return new string
 
 
 
